60f5573f9dd45c2ebca251ebccc2a03b6a65c802
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2023-04-04 23:07:07 +0700

    define store

---
+++ src/store/UserStore.ts
@@ -0,0 +1,8 @@
+import { defineStore } from "pinia";
+
+export const useUserStore = defineStore("UserStore", {
+  state: () => {
+    return {};
+  },
+//   define store https://pinia.vuejs.org/core-concepts/
+});
