fea95118eed2d4bc99b5e3559ddc5f7841da7200
010102tranvanhieu <hieu.tran23@student.passerellesnumeriques.org>
2023-04-05 17:29:49 +0700

    add not found view

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/LoginView.vue";
 import UserView from "../views/UserView.vue";
 import AboutView from "../views/AboutView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
 import Auth from "@/helpers/auth";
 const routes: Array<RouteRecordRaw> = [
   {
@@ -36,6 +37,12 @@ const routes: Array<RouteRecordRaw> = [
     meta: {
       requiresAuth: true,
     },
+  },
+  {
+    path: "/not",
+    name: "notfound",
+    component: NotFoundView,
+    meta: { requiresAuth: false },
   },
 ];
 
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <h1>Not found</h1>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "UserView",
+});
+</script>
