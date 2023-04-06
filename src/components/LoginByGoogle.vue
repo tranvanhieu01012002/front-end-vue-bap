80e884c76158c439840be83049c5c99e3eddf383
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2023-04-06 22:15:14 +0700

    add logout

--- src/components/LoginByGoogle.vue
+++ src/components/LoginByGoogle.vue
@@ -1,11 +1,19 @@
 <template>
   <button class="btn btn-primary">Login by Google</button>
-  <GoogleLogin :callback="callback" />
+  <GoogleLogin :callback="callback" prompt auto-login />
+  <button @click="logout" class="btn btn-success">logout</button>
 </template>
 <script setup lang="ts">
+import { googleLogout } from "vue3-google-login";
+
 const callback = (response: any) => {
   // This callback will be triggered when the user selects or login to
   // his Google account from the popup
   console.log("Handle the response", response);
+};
+
+const logout = (): void => {
+  googleLogout();
+  alert("logout");
 };
 </script>
