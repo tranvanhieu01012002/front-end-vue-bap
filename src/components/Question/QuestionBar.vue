8ef34d1d3fd96fbea5cc4461f2fc5f8eef8c476d
010102tranvanhieu <hieu.tran23@student.passerellesnumeriques.org>
2023-04-14 16:08:17 +0700

    create show current question

---
+++ src/components/Question/QuestionBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>
+    Total questions: {{ currentQuestionId }}/ {{ handleShowAllQuestions }}
+  </h2>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import { mapState } from "pinia";
+import { useQuestionStore } from "@/store";
+export default defineComponent({
+  computed: {
+    ...mapState(useQuestionStore, ["currentQuestionId", "questions"]),
+    handleShowAllQuestions() {
+      return this.questions.length;
+    },
+  },
+});
+</script>
--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -1,13 +1,18 @@
 <template>
-  <h2>Result page</h2>
+  <div class="d-flex justify-content-between">
+    <h2>Result page</h2>
+    <QuestionBar />
+  </div>
   <RankPage />
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import RankPage from "@/components/Ranking/RankPage.vue";
+import QuestionBar from "@/components/Question/QuestionBar.vue";
 export default defineComponent({
   components: {
     RankPage,
+    QuestionBar,
   },
 });
 </script>
