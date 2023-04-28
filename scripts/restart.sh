8c6734443f228fba15d5bfd7e5c8dab94088697c
010102tranvanhieu <hieu.tran23@student.passerellesnumeriques.org>
2023-04-28 08:50:07 +0700

    re update permission

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 sudo systemctl restart nginx
-chmod -R 755 scripts
-chmod -R 755 appspec.yml
+chmod -R 750 /var/www/game.g-learning.click/scripts
+chmod 755  /var/www/game.g-learning.click/scripts appspec.yml
