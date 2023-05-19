302d65e761f7266ddb30fb8a64c32359e1629e86
010102tranvanhieu <hieu.tran23@student.passerellesnumeriques.org>
2023-05-19 08:46:46 +0700

    fix: error when unit test

--- src/tests/helpers/imports.test.ts
+++ src/tests/helpers/imports.test.ts
@@ -2,7 +2,7 @@ import { describe, test, expect } from "vitest";
 
 describe("test import vue component", () => {
   test("test normal import", async () => {
-    const aboutView = await import("../../views/AboutView.vue");
-    expect(aboutView).toBeDefined();
+    const navLink = await import("@/components/NavLink.vue");
+    expect(navLink).toBeDefined();
   });
 });
